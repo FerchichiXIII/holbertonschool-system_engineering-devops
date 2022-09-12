6c54578b7fc2178d3f4af9b3637f7e38db559379
FerchichiXIII <anasferchichi22@gmail.com>
2022-09-12 20:27:31 +0100

    fix pep8

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,9 +9,9 @@ import sys
 
 if __name__ == "__main__":
 
-    user_id =sys.argv[1]
+    user_id = sys.argv[1]
     user = requests.get("https://jsonplaceholder.typicode.com/todos/users/{}"
-                .format(user_id))
+                        .format(user_id))
     name = user.json().get('name')
     todos = requests.get("https://jsonplaceholder.typicode.com/todos")
     total_tasks = 0
@@ -19,11 +19,13 @@ if __name__ == "__main__":
 
     for task in todos.json():
         if task.get('user_id') == int(user_id):
-            total_tasks =+ 1
+            total_tasks = + 1
             if task.get('completed'):
-                completed +=1
-    
-    print('Employee {} is done with task({}/{}):'.format(name, completed, total_tasks))
-    
+                completed += 1
+
+    print('Employee {} is done with task({}/{}):'
+          .format(name, completed, total_tasks))
+
     print('\n'.join(["\t " + task.get("title") for task in todos.json()
-        if task.get("user_id") == int(user_id) and task.get("completed")]))
+                     if task.get("user_id") == int(user_id)
+                     and task.get("completed")]))
