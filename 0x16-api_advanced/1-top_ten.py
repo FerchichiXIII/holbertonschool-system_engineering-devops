f836b48a82612af6428ec0ec9ceb985c819e8bbb
FerchichiXIII <anasferchichi22@gmail.com>
2022-09-27 14:49:16 +0100

    top 10

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+queries the Reddit API and prints the titles
+of the first 10 hot posts listed for a given subreddit.
+"""
+
+import requests
+
+
+def top_ten(subreddit):
+	"""
+	top ten
+	"""
+	url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+	headers = {'User-Agent': 'My User Agent 1.0'}
+	response = requests.get(url, headers=headers, allow_redirects=False)
+	if response.status_code == 200:
+		for i in range(10):
+			print(response.json().get('data').get('children')[i].get('data')
+				  .get('title'))
+	else:
+		print(None)
