9b4fc7c98529b0bbce9a8d19638e5b8d9cf966e2
root <root@DESKTOP-L7AG08S.localdomain>
2022-10-02 11:11:41 +0100

    test

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # find out why Apache is returning a 500 error.
-exec {  'fix-typo':
-  command => "sed -i 's/.phpp/php' /var/www/html/wp-settings.php",
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+exec { 'fix_phpp':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => ['/bin', '/usr/bin/', '/usr/loca/bin/'],
 }
