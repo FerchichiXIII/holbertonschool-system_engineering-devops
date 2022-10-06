9269490d9e25714f658db7900f80db45d4bc1c41
FerchichiXIII <anasferchichi22@gmail.com>
2022-10-06 12:43:12 +0100

    Sky is the limit

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,10 @@
 # testing how well our web server setup featuring Nginx is doing under pressure and it turns out it’s not doing well
 
-exec { 'apt-get update':
-  command => '/usr/bin/apt-get update',
-  path    => '/usr/bin:/bin',
-  onlyif  => '/usr/bin/test ! -e /var/lib/apt/periodic/update-success-stamp -o /var/lib/apt/periodic/update-success-stamp -ot 86400',
+exec {'fix-for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin:/bin',
+}
+exec {'nginx-restart':
+  command => 'service nginx restart',
+  path    => '/etc/init.d/',
 }
