c649e66201d0c7133dda3c8b9c8ac21e7d206a2d
FerchichiXIII <anasferchichi22@gmail.com>
2022-10-06 13:29:33 +0100

    User limit

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,3 +1,5 @@
+# Change the OS configuration so that it is possible to login as root
+
 exec { 'increase-hard-file-limit-for-holberton-user':
   command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
