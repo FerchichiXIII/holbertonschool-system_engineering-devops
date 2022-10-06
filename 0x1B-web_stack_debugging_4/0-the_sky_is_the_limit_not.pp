ac2aad1c9d90d248e6ac15b85deeedfd946a314d
FerchichiXIII <anasferchichi22@gmail.com>
2022-10-06 13:05:57 +0100

    update

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,10 +1,11 @@
 # testing how well our web server setup featuring Nginx is doing under pressure and it turns out it’s not doing well
 
-exec {'fix-for-nginx':
+exec { 'fix--for-nginx':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => '/usr/local/bin:/bin',
-}
-exec {'nginx-restart':
-  command => 'service nginx restart',
-  path    => '/etc/init.d/',
+  path    => '/usr/local/bin/:/bin/'
+  }
+
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
